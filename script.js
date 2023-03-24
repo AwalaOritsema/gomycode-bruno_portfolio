5ba20cbf7c2a7ccd095fc8feb1d431bda087442a
Michael Peter <michaelhpet@gmail.com>
2023-03-24 20:41:32 +0100

    hamburger menu w/ menu container hide/show

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const menuIcon = document.querySelector(".menu-icon")
 const menuContainer = document.querySelector(".menu-container")
 
 menuIcon.onclick = function () {
-  menuContainer.style.display = "flex"
+  menuContainer.style.transform = "translate(0, 0)"
 }
 
 window.onclick = function (event) {
   const elementClicked = event.target
-  // if (elementClicked !== menuContainer && elementClicked !== menuIcon) {
-  //   menuContainer.style.display = "none"
-  // }
+  if (
+    elementClicked !== menuContainer &&
+    elementClicked !== menuIcon &&
+    elementClicked.parentNode !== menuContainer &&
+    elementClicked.parentNode !== menuIcon
+  ) {
+    menuContainer.style.transform = "translate(200%, -400%)"
+  }
 }
